6c6dfa47a6e5953189f70447a7e77e926dc5d326
Glenn Harmon <gharmon@covermymeds.com>
2019-06-17 15:50:20 -0400

    foreign key reference update

--- db/migrate/20190610024708_create_messages.rb
+++ db/migrate/20190610024708_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration[5.1]
   def change
     create_table :messages do |t|
       t.boolean :rsvp
-      t.references :user_event, foreign_key: true
+      t.references :participant, foreign_key: true
       t.boolean :is_paid
 
       t.timestamps
